0fec11b9fd595cfcac2c8df71b87c24be5d08861
Glamurio <ryou.takeshi@gmail.com>
2023-12-30 00:00:07 +0000

    Add preliminary context menu

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 $( document ).ready(function() {
+
+    /* Context Menu */
+    $('#main-container').on('contextmenu', function(event) {
+        console.log('check')
+        event.preventDefault();
+        $('#context-menu').removeClass('hide')
+        $('#context-menu')[0].style.top = mouseY(event) + 'px';
+        $('#context-menu')[0].style.left = mouseX(event) + 'px';
+
+        window.event.returnValue = false;
+    });
+
+    function mouseX(evt) {
+        if (evt.pageX) {
+            return evt.pageX;
+        } else if (evt.clientX) {
+            return evt.clientX + (document.documentElement.scrollLeft ?
+            document.documentElement.scrollLeft :
+            document.body.scrollLeft);
+        } else {
+            return null;
+        }
+    }
+      
+    function mouseY(evt) {
+        if (evt.pageY) {
+            return evt.pageY;
+        } else if (evt.clientY) {
+            return evt.clientY + (document.documentElement.scrollTop ?
+            document.documentElement.scrollTop :
+            document.body.scrollTop);
+        } else {
+            return null;
+        }
+    }
+
     // Window Function
     let lastWindowPos
     let windowTemplate = `
@@ -7,10 +43,11 @@ $( document ).ready(function() {
         <button class="close-btn">X</button>
     </div>
     <div class="window-content-wrapper">
-        <div class="window-content">
-        </div>
+        <textarea class="window-content">
+        </textarea>
     </div>
     `
+
     $('.icon-wrapper').on('dblclick', function(event) {
         let windowContainer = document.createElement("div");
         windowContainer.classList = 'window-container'
@@ -26,9 +63,11 @@ $( document ).ready(function() {
         }
         lastWindowPos = $(windowContainer).offset();
         const id = $(this).find('img').attr('id')
+        const icon = $(this).find('img').attr('src')
         switch(id) {
             case 'about-me':
-                $(windowContainer).find('.window-title').html('About Me')
+                tabHtml = `<img class="icon" src="${icon}" alt="About Me"> About Me`
+                $(windowContainer).find('.window-title').html(tabHtml)
                 windowContent = "It's me!"
                 break;
             default:
@@ -52,7 +91,8 @@ $( document ).ready(function() {
         const resizeOptions = {
             minHeight: 150,
             minWidth: 200,
-            containment: '#main-container'
+            containment: '#main-container',
+            alsoResize: $(this).find(".window-content-wrapper, .window-content"),
         }
         $('.window-container').resizable(resizeOptions);
         $('.window-container').on('click', function() {
@@ -71,13 +111,20 @@ $( document ).ready(function() {
 
     // Init Icons
     initIcons = () => {
+        const dragOptions = {
+            containment: '#main-container',
+            grid: [ 100, 100 ]
+        }
+        $( '.icon-wrapper' ).draggable(dragOptions)
         $( '.icon-wrapper' ).on('click', function(event) {
             event.stopPropagation();
             $(this).find('.icon-container').addClass('selected')
         })
-        $(document).on('click', function() {
-            $('.selected').removeClass('selected');
-        });
     }
     initIcons()
+
+    $(document).on('click', function() {
+        $('.selected').removeClass('selected');
+        $('#context-menu').addClass('hide');
+    });
 });
